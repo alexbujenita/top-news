87f869b194dbd4006edebba0e531a966e00d1735
Alex Bujenita <dekc89@gmail.com>
2020-02-02 11:36:38 +0000

    initial react app boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div>
+      <p className={styles.test}>
+        CSS modules test
+      </p>
+    </div>
+  );
+}
+
+export default App;
